1b281f3eb0819d5faf2eac2b285cc82d34827f55
swh <sungwoo166@gmail.com>
2021-09-30 15:57:30 +0900

    Polls 기능 추가

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render, Http404, get_object_or_404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
 
 #request 는 클라이언트에서 받음
 def index(request):
@@ -14,10 +15,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message': "You didn't select a choice.",
+
+        })
+    else:
+        select_choice.votes += 1
+        select_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
