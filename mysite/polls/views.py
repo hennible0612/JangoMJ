5db366f658f68f748399194843f7f71ebe84242a
swh <sungwoo166@gmail.com>
2021-09-30 12:22:03 +0900

    404에러 테스트
    .

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,28 @@
+from django.http import HttpResponse, Http404
+from django.template import loader
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
 from .models import Question
+
 #request 는 클라이언트에서 받음
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = { 'latest_question_list': latest_question_list, }#context에 html 에 넣을 데이터 정리
+    return render(request,'polls/index.html', context) #context와 넘겨줌
+
+
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+
+
 
 def detail(request, question_id):
-     return HttpResponse("You're looking at question %s." %question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
