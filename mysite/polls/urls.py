53797f462ad6102f10f47a83feb9ece742ba2ed8
swh <sungwoo166@gmail.com>
2021-10-01 17:31:06 +0900

    urls views 수정

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,16 +1,29 @@
+# from django.urls import path
+# from . import views
+#
+#
+# app_name = 'polls' #하드 코딩된 url제거용가리
+# urlpatterns = [
+#     # /polls/
+#     path('', views.index, name='index'),
+#     # /polls/5/
+#     path('<int:question_id>/',views.detail, name='detail'),
+#     # /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # /polls/5/vote
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+#
+# ]
+
+#제너릭 뷰
 from django.urls import path
+
 from . import views
 
-
-app_name = 'polls' #하드 코딩된 url제거용가리
+app_name = 'polls'
 urlpatterns = [
-    # /polls/
-    path('', views.index, name='index'),
-    # /polls/5/
-    path('<int:question_id>/',views.detail, name='detail'),
-    # /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # /polls/5/vote
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,22 +1,52 @@
-from django.http import HttpResponse, HttpResponseRedirect
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.shortcuts import get_object_or_404, render
+# from django.urls import reverse
+#
+# from .models import Choice, Question
+#
+# #request 는 클라이언트에서 받음
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = { 'latest_question_list': latest_question_list, }#context에 html 에 넣을 데이터 정리
+#     return render(request,'polls/index.html', context) #context와 넘겨줌
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+#
+
+
+#제너릭뷰
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-#request 는 클라이언트에서 받음
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = { 'latest_question_list': latest_question_list, }#context에 html 에 넣을 데이터 정리
-    return render(request,'polls/index.html', context) #context와 넘겨줌
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -33,5 +63,3 @@ def vote(request, question_id):
         select_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
