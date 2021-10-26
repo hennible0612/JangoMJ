dd7816567c7e25a486eda6a7e3f7cd70a2c910a5
swh <sungwoo166@gmail.com>
2021-10-26 14:00:57 +0900

    DB
    .

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -13,15 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# import sys
-# sys.path.append('Users\henni\Desktop\JangoMJ\mysite\pybo')
 from django.contrib import admin
-from django.urls import path
-# from mysite.pybo import views
-from pybo import views
-# from ..pybo import views
+from django.urls import path, include
+# from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', views.index),
+    path('pybo/', include('pybo.urls')),
+    path('', include('testmain.urls'))
 ]
---
+++ mysite/testmain/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestmainConfig(AppConfig):
+    name = 'testmain'
